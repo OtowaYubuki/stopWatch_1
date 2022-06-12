200ed3ac37f620e9ed042b52ac410c87fec551e0
EC2 Default User <ec2-user@ip-172-31-0-224.us-west-2.compute.internal>
2022-06-12 12:04:05 +0000

    first commit

--- test1.js
+++ test1.js
@@ -1,9 +1,6 @@
 let timer = document.getElementById("timer");
-
 let start = document.getElementById("start");
-
 let stop = document.getElementById("stop");
-
 let reset = document.getElementById("reset");
 
 let hours = 0;
@@ -11,21 +8,42 @@ let minutes = 0;
 let seconds_1 = 0;
 let seconds_2 = 0;
 
-timer.innerHTML = hours + ":" + minutes + ":" + seconds_1 + ":" + seconds_2;
+let interval;
 
-function push(){
-    seconds_2++;
-if(seconds_2 > 9)
+function stopWatch(){
+ seconds_2++;
+if(seconds_2 > 9){
     seconds_1++;
     seconds_2 = 0;
- 
-if(seconds_1 > 9)
+}
+if(seconds_1 > 9){
     minutes++;
     seconds_1 = 0;
-
-if(minutes > 9)
+}
+if(minutes > 9){
     hours++; 
     minutes = 0;
+}    
+timer.innerHTML = hours + ":" + minutes + ":" + seconds_1 + ":" + seconds_2;
 }
-push();
-setInterval(push,100);
+ 
+start.addEventListener("click",function(){
+interval = setInterval(stopWatch,100);
+start.disabled = true;
+})
+
+stop.addEventListener("click",function(){
+clearInterval(interval);
+stop.disabled = true;
+start.disabled = false;
+})
+    
+reset.addEventListener("click",function(){
+    clearInterval(interval);
+    timer.innerHTML = "0:0:0:0";
+    stop.disabled = false;
+    start.disabled = false;
+})
+
+
+
